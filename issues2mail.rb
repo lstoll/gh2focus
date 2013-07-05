99510513ee0ad7bb30129422e42327db7557290f
Bench User <bench@example.com>
2013-07-05 20:06:28 +0200

    logic around getting issues and seeing if processed

--- issues2mail.rb
+++ issues2mail.rb
@@ -7,7 +7,9 @@ Dotenv.load
 ## Setup
 
 DB = Sequel.connect(ENV['DATABASE_URL'])
-DB.create_table?(:processed) {primary_key :id}
+DB.create_table?(:processed) {String :issue}
+# Ignore errors sucks, but no 'create unless'
+DB.add_index :processed, :issue, :ignore_errors => true
 
 Pony.options = {
   :via => :smtp,
@@ -26,8 +28,15 @@ GH = Octokit::Client.new(:login => ENV['GITHUB_USER'], :oauth_token => ENV['GITH
 
 ## Do it
 
-pp GH.issues.first
+issue = GH.issues.first
 
-# GH.issues.each do |issue|
-
-# end
+GH.issues.each do |issue|
+  issue_id = "#{issue[:repository][:name]}/#{issue[:number]}"
+  DB[:processed].where('issue = ?', issue_id).count
+  unless DB[:processed].where('issue = ?', issue_id).count > 0
+    issue_title = issue[:title]
+    issue_body = issue[:body]
+    puts "DO THE THING"
+    DB[:processed].insert(:issue => issue_id)
+  end
+end
